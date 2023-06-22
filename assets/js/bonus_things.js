cdff3a1f1f21d551a9c6b944a243df5afbb7b837
VeeIsForVanana <95967340+VeeIsForVanana@users.noreply.github.com>
2023-06-23 07:43:21 +0800

    Revised wording of conclusion, added weird unrolling thing for complicated terms.
    Changed the wording of the conclusion to be more accessible to laypersons. Also added this effect for certain terms or figures in text that could be elaborated on.

--- assets/js/bonus_things.js
+++ assets/js/bonus_things.js
@@ -1,5 +1,12 @@
 const font_awesome = document.querySelectorAll('.fa');
+const unrollables = document.querySelectorAll('.unroll');
 
+// this map is structured ["<element id>", ["<og text>", "<unrolled text>"]]
+const unrollables_map = {
+  "unroll-1": ["1.47e-10", "(that's 0.000000000147!)"],
+  };
+
+// Handling the pulsable font-awesomes
 
 function getClicked () {
   this.classList.add('fa-pulse');
@@ -11,3 +18,16 @@ function offClicked () {
 
 font_awesome.forEach(li => li.addEventListener('mouseover', getClicked));
 font_awesome.forEach(li => li.addEventListener('mouseout', offClicked));
+
+// Handling the unrollables
+
+function unroll () {
+  this.innerText = unrollables_map[this.id][1];
+}
+
+function roll () {
+  this.innerText = unrollables_map[this.id][0];
+}
+
+unrollables.forEach(li => li.addEventListener('mouseover', unroll));
+unrollables.forEach(li => li.addEventListener('mouseout', roll));
